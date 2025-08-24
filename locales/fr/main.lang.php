c7a1b02aa6e16466beb8c9c141e74022050fbea7
Jean-Christian Denis <contact@jcdenis.fr>
2025-08-24 18:21:19 +0200

    add fr translation

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,27 @@
+<?php
+// Language: Français
+// Module: CredentialsRecords - 0.1.2
+// Date: 2025-08-24 16:19:10
+// Author: , contact@jcdenis.fr
+// Translated with Translater - 2025.03.03
+
+
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Credential'] = 'Jeton';
+L10n::$locales['Data'] = 'Données';
+L10n::$locales['No credential matches the filter'] = 'Aucun jeton correspondant au filtre';
+L10n::$locales['No credential'] = 'Aucun jeton';
+L10n::$locales['List of %s credentials matching the filter.'] = 'Liste des %s jetons correspondant au filtre.';
+L10n::$locales['List of credentials. (%s)'] = 'Listes des jetons. (%s)';
+L10n::$locales['[EMPTY]'] = '[VIDE]';
+L10n::$locales['Selected credentials have been successfully deleted'] = 'Les jetons sélectionnés ont été effacé avec succès';
+L10n::$locales['Are you sure you want to delete selected credentials?'] = 'Etes vous sur de vouloir effacer les jetons sélectionnés ?';
+L10n::$locales['There are no credentials'] = 'Il n\'y a pas de jetons';
+L10n::$locales['Delete selected credentials'] = 'Effacer les jetons sélectionnés';
+L10n::$locales['Credential:'] = 'Jeton :';
+L10n::$locales['Credentials'] = 'Jetons';
